756562139d1ba11df0ce773636b76eb36df75d1f
viktorstepanov <stepanov84@yahoo.com>
2022-09-26 21:07:54 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,17 @@
 ﻿//int numberA = 3;
 //int numberB = 5;
 //Console.WriteLine(numberA + numberB);
-int numberA = 3;
-int numberB = 5;
+
+
+//int numberA = 3;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+
+int numberA = new Random().Next(1, 10); //0 1 2 ....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); //0 1 2 ....9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
