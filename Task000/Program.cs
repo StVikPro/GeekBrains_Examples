6a6adf7c049d9dbacd26066195f199064a60d51e
viktorstepanov <stepanov84@yahoo.com>
2022-10-03 23:24:25 +0300

    Task000 - нулевая задача на лекции 1

---
+++ Task000/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число и выдает
+//его квадрат (число умножает на само себя)
+//Вариант 1
+//Console.Clear();
+//Console.WriteLine("Введите число: ");
+//string input = Console.ReadLine();
+//int number = int.Parse(input);
+//Console.WriteLine(number*number);
+//Вариант 2
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int  sqr = number * number;
+int  sqr2 = (int)Math.Pow(number,2);//(int) - в скобках перед присваивает значение Инт переменной
+Console.WriteLine(sqr);
+Console.WriteLine(sqr2);
