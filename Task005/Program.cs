d15fb4eed62f7dd20f3787574752a2f0852b9edc
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 00:29:48 +0300

    Task005 третья задача в группах на семинаре

---
+++ Task005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+//а на выходе показывает все целые числа в промежутке от -N до N
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = -1 * num;
+
+while(count <= num)
+{
+    Console.Write($"{count} ");
+    count ++;
+}
+
