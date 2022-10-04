b69313377ac3af35b2c59ead9a0d9e86ced0d2e4
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 22:38:31 +0300

    Task008 семинары 8 задача

---
+++ Task009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 9 Напишите программу, которая выводит случайное число 
+// из отрезка [10,99] и показывает наибольшую цифру числа
+Console.Clear();
+
+int number = new Random().Next(10,100);// генерируем число оператором
+
+Console.WriteLine(number);//выводим
+//Далее разбиваем числа на цифры первое и второе
+int firstDigit = number % 10; //поиск младшего разряда числа это остаток от деления на 10
+int lastDigit = number/10; //остаток 
+
+if (firstDigit > lastDigit)
+    Console.WriteLine(firstDigit);
+else 
+    Console.WriteLine(lastDigit);
