1a9ce995f064572341397372a8f07315ac6a1361
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 21:51:39 +0300

    Task006 домашняя работа задача 6 первый семинар

---
+++ Task006Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6 Напишите программу, которая на вход принимет число
+// и выдает, является ли число четным (делится ли оно на два 
+// без остатка)
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
