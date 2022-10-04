b0452591d960f6171523f4788c2a9fd91ce39e58
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 22:02:47 +0300

    Task008 домашняя работа задача 8 первый семинар

---
+++ Task008Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8 Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int counter = 2;
+
+while (n > counter)
+{
+    Console.Write($"{counter} ");
+    counter += 2;
+}
