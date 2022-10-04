7a443e9c5d07db9f2dd98d91d2bb7794e6e6b614
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 21:19:37 +0300

    Task002 домашняя работа задача первый семинар

---
+++ Task002Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 2 Напишите программу, которая на вход принимает два числа
+// и выыдает, ккое число больше, а какое меньше
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}, min = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {b}, min = {a}");
+}
