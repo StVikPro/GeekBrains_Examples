406cc85198abb318663575d5fded69176aa07e1b
viktorstepanov <stepanov84@yahoo.com>
2022-10-04 23:00:36 +0300

    Task011 семинары 11 задача

---
+++ Task011/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 11 Напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int firstDigit = number / 100; //поиск первого числа
+int lastDigit = number % 10; //остаток второе число
+
+Console.WriteLine((firstDigit * 10) + lastDigit);
