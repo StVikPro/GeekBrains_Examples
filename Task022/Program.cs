75ace2ea37ce76c2e16ae74812d0c66e1d8ab57b
viktorstepanov <stepanov84@yahoo.com>
2022-10-06 17:53:56 +0300

    Task022 задача 22 семинары решение в группах

---
+++ Task022/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 22 Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел он 1 до N
+// 5 -> 1,4,9,16,25
+// 2 -> 1,4
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 2)}");
+    if (count !=n)
+        Console.Write(", ");
+        count++;
+}
