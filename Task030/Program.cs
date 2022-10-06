bdd04b364eb55bb9f0df3107e6e66c5eca9be4fd
viktorstepanov <stepanov84@yahoo.com>
2022-10-06 19:30:36 +0300

    Task030 задача 30 семинар преподаватель решает задачу

---
+++ Task030/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 30 Напишите программу, число, создает массив данного размера,
+заполняет его нулями и единицами в случайном порядке и выводит его в консоль
+8 ->[1,0,1,1,0,1,0,0] */
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int arrayLenght = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[arrayLenght];
+
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(int[] array)
+{
+    Random randGeneration = new Random();
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        array [i] = randGeneration.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
