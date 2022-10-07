45c7243bc9fae59e28a952041be7caa29334362f
viktorstepanov <stepanov84@yahoo.com>
2022-10-07 16:52:35 +0300

    Task034Homework  задача 34 домашние задачи

---
+++ Task034Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+Console.Clear();
+
+int[] array = new int[8];
+
+Random randomGenerator = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randomGenerator.Next(100, 1000);
+}
+
+int countOfEven = CountEver(array);
+
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine(countOfEven);
+
+
+
+int CountEver(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] % 2 ==0)
+            counter++; 
+    }
+    return counter;
+}
