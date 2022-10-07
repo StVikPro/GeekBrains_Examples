987186fee5d5cb30303116ebfc7be236307259ac
viktorstepanov <stepanov84@yahoo.com>
2022-10-07 17:59:21 +0300

    Task039New задача 39 домашние семинары разбор преподавателя

---
+++ Task039New/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 39 Напишите программу, которая принимает на вход число
+(А) и выдает сумму чисел от 1 до А.
+Использовать рекурсию
+7 -> 28
+4 -> 10
+8 -> 36 */
+Console.Clear();
+
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Sum: {RecSum(number)}");
+
+
+int RecSum(int number)
+{
+    if (number > 1)
+    {
+        return number + RecSum(number - 1);
+    }
+    else
+    { 
+        return 1;
+    }
+}
