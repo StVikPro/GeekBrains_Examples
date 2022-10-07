8604baaa5a489b06a3dc1f6b4bd6c5668786c367
viktorstepanov <stepanov84@yahoo.com>
2022-10-07 13:46:32 +0300

    Task027 задача 27 домашнее задание

---
+++ Task027Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵);   2, 4 -> 16   */
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Cумма всех цифр в числе равна: " + sum);
