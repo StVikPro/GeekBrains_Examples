c08bc3b6d0ff8b79d76608e42ff70715d4912df7
viktorstepanov <stepanov84@yahoo.com>
2022-10-07 12:38:16 +0300

    Task035 задача 35 семинар решение задач в группах

---
+++ Task035/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 35 Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в 
+отрезке [10,99]. (Пример для массива 5 элементов [5,18,123,6,2] -> 1)
+[10,11,12,13,14 -> 5]
+ */
+ Console.Clear();
+ int[] array = new int[123];
+ FillArray(array);
+ int c = GetCount(array);
+ Console.WriteLine($"Количество элементов от 10 до 99 равно {c} ");
+
+ int GetCount(int[] arr)
+ {
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        count ++;
+    }
+    return count;
+ }
+
+void FillArray(int[] array)
+{
+    Random rendGeneration = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rendGeneration.Next(0, 201);
+    }
+}
