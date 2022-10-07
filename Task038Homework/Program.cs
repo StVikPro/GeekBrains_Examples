d180c3fe9a6e91c0e93466a1b451fb8485c2db79
viktorstepanov <stepanov84@yahoo.com>
2022-10-07 17:45:47 +0300

    Task038Homework задача 38 домашние задачи

---
+++ Task038Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+между максимальным и минимальным элементом массива.
+[3 7 22 2 78] -> 76 */
+Console.Clear();
+
+double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+        if (minNumber > arrayRealNumbers[i])
+    {
+      minNumber = arrayRealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"\nРазница между между максимальным и минимальным элементами массива: {decision}");
