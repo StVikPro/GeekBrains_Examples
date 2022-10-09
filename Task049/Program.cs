abb78f3a742648dc7ad777e78a66dec44fff266d
viktorstepanov <stepanov84@yahoo.com>
2022-10-09 19:25:41 +0300

    Task049 задача 49 решение задач в группах

---
+++ Task049/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых 
+оба индекса чётные, и замените эти элементы на их квадраты */
+
+Console.Clear();
+
+int[,] array = new int[5, 5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("");
+ReplaceArray(array);
+PrintArray(array);
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+
+void ReplaceArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i +=2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+          //array[i,j] = array[i,j] *  array[i,j]
+          array[i,j] = (int)Math.Pow(array[i,j] , 2);
+        }
+    }
+}
+
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          array[i,j] = generator.Next(1, 10);
+        }
+    }
+}
